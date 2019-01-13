afb5c31c6fb2402f1a83c114c24f3ebbcafca7b7
UJamil <usmanajamil@gmail.com>
2019-01-12 17:21:20 -0800

    new commit

--- Voltage-Sensing-Test/src/main.cpp
+++ Voltage-Sensing-Test/src/main.cpp
@@ -1,10 +1,28 @@
 #include <mbed.h>
 
-int main() {
+int main()
+{
 
   // put your setup code here, to run once:
+  AnalogIn ain(A0);
+  DigitalOut dout(LED1);
+  while (1)
+  {
+    //test the voltage on the initialized analog pin
+    //  and if greater than 0.3 * VCC set the digital pin
+    //  to a logic 1 otherwise a logic 0
+    if (ain > 0.3f)
+    {
+      dout = 1;
+    }
+    else
+    {
+      dout = 0;
+    }
 
-  while(1) {
-    // put your main code here, to run repeatedly:
+    // print the percentage and 16 bit normalized values
+    printf("percentage: %3.3f%%\n", ain.read() * 100.0f);
+    printf("normalized: 0x%04X \n", ain.read_u16());
+    wait(0.2f);
   }
 }
