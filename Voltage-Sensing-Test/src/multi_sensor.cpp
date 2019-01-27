a79d7c5f34d41b5fb12a2d4f6252b515b88d4508
UJamil <usmanajamil@gmail.com>
2019-01-27 15:14:18 -0800

    added another channel

--- Voltage-Sensing-Test/src/multi_sensor.cpp
+++ Voltage-Sensing-Test/src/multi_sensor.cpp
@@ -9,14 +9,15 @@ int main()
 {
 
   // put your setup code here, to run once:
-  AnalogIn ain(A0);
+  AnalogIn ain0(A0);
+  AnalogIn ain1(A1);
   DigitalOut dout(LED1);
   while (1)
   {
     //test the voltage on the initialized analog pin
     //  and if greater than 0.5 * VCC set1the digital pin
     //  to a logic 1 otherwise a logic 0
-    if (ain > 0.5f)
+    if (ain0 > 0.5f)
     {
       dout = 1;
     }
@@ -26,7 +27,7 @@ int main()
     }
 
     // print the percentage and 16 bit normalized values
-    printf ("voltage: %3.9f\n", ain.read() * 3.3f);
+    printf ("voltage: %3.9f\n", ain0.read() * 3.3f);
 
     // printf("percentage: %3.9f%%\n", ain.read() * 100.0f);
     // printf("normalized: 0x%04X \n", ain.read_u16());
