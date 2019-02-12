51aaf839b3f21a1636d68cb89a17012f75359fe6
UJamil <usmanajamil@gmail.com>
2019-02-11 23:27:17 -0800

    Added more sensors

--- Voltage-Sensing-Test/src/main.cpp
+++ Voltage-Sensing-Test/src/main.cpp
@@ -4,28 +4,22 @@ int main()
 {
 
   // put your setup code here, to run once:
-  AnalogIn ain(A0);
-  DigitalOut dout(LED1);
+  AnalogIn Battery1_VBatt(A0); //
+  AnalogIn Battery2_VBatt(A1); //
+  AnalogIn Battery3_VBatt(A2); //
+
+  // To be assigned
+  // AnalogIn Battery1_VShunt(A3); 
+  // AnalogIn Battery1_VShunt(A3);
+  // AnalogIn Battery1_VShunt(A3);
+
   while (1)
   {
-    //test the voltage on the initialized analog pin
-    //  and if greater than 0.5 * VCC set1the digital pin
-    //  to a logic 1 otherwise a logic 0
-    if (ain > 0.5f)
-    {
-      dout = 1;
-    }
-    else
-    {
-      dout = 0;
-    }
-
     // print the percentage and 16 bit normalized values
-    printf ("voltage: %3.9f\n", ain.read() * 3.3f);
-
+    printf("Batt 1 V: %3.4f V\tBatt 2 V: %3.4f V\tBatt 3 V: %3.4f V\n", Battery1_VBatt.read() * 3.3f, Battery2_VBatt.read() * 3.3f, Battery3_VBatt.read() * 3.3f);
     // printf("percentage: %3.9f%%\n", ain.read() * 100.0f);
     // printf("normalized: 0x%04X \n", ain.read_u16());
 
-    // wait(0.1f);
+    wait(0.1f); //wait 0.1ms, corresponding to 10kHz
   }
 }
